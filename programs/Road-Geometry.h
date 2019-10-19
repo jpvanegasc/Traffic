a617acadddb0007e35cb4c7cf2d19e53cca7e0be
Juan Pablo Vanegas <juan.vanegas@tektontech.com>
2019-10-18 19:33:59 -0500

    Feat: Implements road geometry module

---
+++ programs/Road-Geometry.h
@@ -0,0 +1,11 @@
+/**
+ * Street Geometry
+ */
+
+//One road, one way for now
+
+const double y_length = 10;
+const double y_width = 2;
+
+const double y_center = 5;
+
--- programs/Vehicle.h
+++ programs/Vehicle.h
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<cmath>
+#include"Road-Geometry.h"
 
 /*Superclass*/
 class Vehicle{
